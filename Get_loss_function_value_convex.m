f48cc4013a89d4efa1389b930ad1b363254e5e82
Sanyogita Purandare <47488309+snygt2007@users.noreply.github.com>
2019-02-10 13:24:57 -0600

    Delete Get_loss_function_value_convex.m

--- Get_loss_function_value_convex.m
+++
@@ -1,9 +0,0 @@
-function [  Output_x1,grad_x1 ] = Get_loss_function_value_convex( x1 )
-%Get_loss_function_value provides the Numerical value of a convex function at x1
-% Input:
-% Variable x1
-% Number of iterations i_iterations
-Output_x1 = x1.^2;
-grad_x1 = 2.*x1;
-end
-
